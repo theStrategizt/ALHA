1c4d52d09577e42edfc53c1a77dcfe216189d747
8mileverse <Ayodelu4@gmail.com>
2025-10-14 23:05:53 +0100

    #feat: minor tweak on download form btn

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 // Adjusted Navbar with responsive sizing for tablet (768px)
 import React, { useState, useRef, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import { ChevronDown, X, Menu } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Links } from "../../constants";
@@ -24,6 +25,8 @@ const Navbar = () => {
     window.addEventListener("keydown", handleEsc);
     return () => window.removeEventListener("keydown", handleEsc);
   }, []);
+
+  // const navigate = useNavigate();
 
   return (
     <header className="fixed top-0 z-50 w-full backdrop-blur-md bg-transparent border-b border-gray-200">
@@ -92,15 +95,18 @@ const Navbar = () => {
         <div className="hidden md:flex items-center space-x-2 sm:space-x-4 md:space-x-6">
           <Button
             variant="black"
-            className="px-3 sm:px-4 md:px-5 lg:px-6 py-1.5 sm:py-2 md:py-1.5 text-sm sm:text-sm md:text-[0.65em] lg:text-[0.65em]"
+            className="px-3 sm:px-4 md:px-5 lg:px-6 py-1.5 sm:py-2 md:py-1.5 text-sm sm:text-sm md:text-[0.65em] lg:text-[0.65em] cursor-pointer"
           >
             APPLY ONLINE
           </Button>
           <Button
+            asChild
             variant="outlineBlack"
             className="px-3 sm:px-4 md:px-5 lg:px-6 py-1.5 sm:py-2 md:py-2.5 text-sm sm:text-sm md:text-[0.65em] lg:text-[0.65em]"
           >
-            DOWNLOAD APPLICATION FORM
+            <a href="/documents/application-Form.pdf" download>
+              DOWNLOAD APPLICATION FORM
+            </a>
           </Button>
         </div>
 
@@ -168,10 +174,13 @@ const Navbar = () => {
               APPLY ONLINE
             </Button>
             <Button
+              asChild
               variant="outlineBlack"
               className=" rounded-md w-full text-sm sm:text-base py-2 sm:py-2.5"
             >
-              DOWNLOAD APPLICATION FORM
+              <a href="/documents/application-Form.pdf" download>
+                DOWNLOAD APPLICATION FORM
+              </a>
             </Button>
           </div>
         </div>
